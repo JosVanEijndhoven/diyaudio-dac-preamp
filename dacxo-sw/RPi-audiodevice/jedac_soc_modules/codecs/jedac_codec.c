757e2c092b959ff3d41780598caad86e17335389
Jos van Eijndhoven <jos@vaneijndhoven.net>
2026-01-26 12:46:39 +0100

    Fix old leftover in codec

--- dacxo-sw/RPi-audiodevice/jedac_soc_modules/codecs/jedac_codec.c
+++ dacxo-sw/RPi-audiodevice/jedac_soc_modules/codecs/jedac_codec.c
@@ -132,9 +132,7 @@ static int codec_hw_params(struct snd_pcm_substream *substream,
 			     struct snd_pcm_hw_params *params,
 			     struct snd_soc_dai *dai)
 {
-	unsigned int rate = params_rate(params);
   struct snd_soc_component *codec = dai->component;
-
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
 	struct snd_soc_dai *cpu_dai = snd_soc_rtd_to_cpu(rtd, 0);
 	int samplerate = params_rate(params);
